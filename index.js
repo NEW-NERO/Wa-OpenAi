c16d66d343dc51a132a92c05dc1c7d5baf4a0845
John <91378451+NEW-NERO@users.noreply.github.com>
2023-04-22 15:42:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "yusril";
-const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const donet = "Hi";
+const owner = ["595972980943"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
